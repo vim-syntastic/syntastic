2c0abf4515d98588a874628b7b1178f7c23403e5
Martin Grenfell <martin.grenfell@gmail.com>
2012-12-03 23:55:54 +0000

    perl: fix some comments that got slightly screwed

--- syntax_checkers/efm_perl.pl
+++ syntax_checkers/efm_perl.pl
@@ -4,7 +4,7 @@ use warnings
 # vimparse.pl - Reformats the error messages of the Perl interpreter for use
 # with the quickfix mode of Vim
 #
-# Copyright (�) 2001 by J�rg Ziefle <joerg.ziefle@gmx.de>
+# Copyright (c) 2001 by Jörg Ziefle <joerg.ziefle@gmx.de>
 # Copyright (c) 2012 Eric Harmon <http://eharmon.net>
 # You may use and distribute this software under the same terms as Perl itself.
 #
