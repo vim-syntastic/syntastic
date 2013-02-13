vim-syntastic/syntastic

commit 653611ccac7f36ee6601d340c60524325db83b25

split up php checker into php, phpmd and phpcs

syntax_checkers/php/phpcs.vim
@@ -1,0 +1,31 @@
+"============================================================================
+"File:        phpcs.vim
+"Description: Syntax checking plugin for syntastic.vim
+"Maintainer:  Martin Grenfell <martin.grenfell at gmail dot com>
+"License:     This program is free software. It comes without any warranty,
+"             to the extent permitted by applicable law. You can redistribute
+"             it and/or modify it under the terms of the Do What The Fuck You
+"             Want To Public License, Version 2, as published by Sam Hocevar.
+"             See http://sam.zoy.org/wtfpl/COPYING for more details.
+"
+"============================================================================
+"
+" See here for details of phpcs
+"    - phpcs (see http://pear.php.net/package/PHP_CodeSniffer)
+"
+function! SyntaxCheckers_php_phpcs_IsAvailable()
+    return executable('phpcs')
+endfunction
+
+function! SyntaxCheckers_php_phpcs_GetLocList()
+    let makeprg = syntastic#makeprg#build({
+                \ 'exe': 'phpcs',
+                \ 'args': '--report=csv',
+                \ 'subchecker': 'phpcs' })
+    let errorformat = '%-GFile\,Line\,Column\,Type\,Message\,Source\,Severity,"%f"\,%l\,%c\,%t%*[a-zA-Z]\,"%m"\,%*[a-zA-Z0-9_.-]\,%*[0-9]'
+    return SyntasticMake({ 'makeprg': makeprg, 'errorformat': errorformat, 'subtype': 'Style' })
+endfunction
+
+call g:SyntasticRegistry.CreateAndRegisterChecker({
+    \ 'filetype': 'php',
+    \ 'name': 'phpcs'})

plugin/syntastic/registry.vim
@@ -6,8 +6,9 @@ let g:loaded_syntastic_registry=1
 let s:defaultCheckers = {
         \ 'c': ['gcc'],
         \ 'cpp': ['gcc'],
-        \ 'objc': ['gcc'],
         \ 'java': ['javac'],
+        \ 'objc': ['gcc'],
+        \ 'php': ['php', 'phpcs', 'phpmd'],
         \ 'ruby': ['mri']
     \ }
 

syntax_checkers/php/php.vim
@@ -10,40 +10,9 @@
 "
 "============================================================================
 "
-"This syntax checker is composed of three checkers:
-"   - php -l
-"   - phpcs (see http://pear.php.net/package/PHP_CodeSniffer)
-"   - phpmd (see http://phpmd.org)
-"
-"If any of these checkers are installed then they will be used. Phpcs and
-"Phpmd are 'style checkers' and will only be called if `php -l` doesnt find
-"any syntax errors.
-"
-"There are options below to config and disable phpcs and phpmd.
-
 function! SyntaxCheckers_php_php_IsAvailable()
     return executable("php")
 endfunction
-
-"Support passing configuration directives to phpcs
-if !exists("g:syntastic_phpcs_conf")
-    let g:syntastic_phpcs_conf = ""
-endif
-
-if !exists("g:syntastic_phpcs_disable") || !executable('phpcs')
-    let g:syntastic_phpcs_disable = 0
-endif
-
-
-if !exists("g:syntastic_phpmd_disable") || !executable('phpmd')
-    let g:syntastic_phpmd_disable = 0
-endif
-
-
-"Support passing selected rules to phpmd
-if !exists("g:syntastic_phpmd_rules")
-    let g:syntastic_phpmd_rules = "codesize,design,unusedcode,naming"
-endif
 
 function! SyntaxCheckers_php_GetHighlightRegex(item)
     let unexpected = matchstr(a:item['text'], "unexpected '[^']\\+'")
@@ -59,38 +28,7 @@ function! SyntaxCheckers_php_php_GetLocList()
                 \ 'args': '-l -d error_reporting=E_ALL -d display_errors=1 -d log_errors=0',
                 \ 'subchecker': 'php' })
     let errorformat='%-GNo syntax errors detected in%.%#,Parse error: %#syntax %trror\ , %m in %f on line %l,Parse %trror: %m in %f on line %l,Fatal %trror: %m in %f on line %l,%-G\s%#,%-GErrors parsing %.%#'
-    let errors = SyntasticMake({ 'makeprg': makeprg, 'errorformat': errorformat })
-
-    if empty(errors)
-        if !g:syntastic_phpcs_disable
-            let errors = errors + s:GetPHPCSErrors()
-        endif
-
-        if !g:syntastic_phpmd_disable
-            let errors = errors + s:GetPHPMDErrors()
-        endif
-    end
-
-    return errors
-endfunction
-
-function! s:GetPHPCSErrors()
-    let makeprg = syntastic#makeprg#build({
-                \ 'exe': 'phpcs',
-                \ 'args': g:syntastic_phpcs_conf . " --report=csv",
-                \ 'subchecker': 'phpcs' })
-    let errorformat = '%-GFile\,Line\,Column\,Type\,Message\,Source\,Severity,"%f"\,%l\,%c\,%t%*[a-zA-Z]\,"%m"\,%*[a-zA-Z0-9_.-]\,%*[0-9]'
-    return SyntasticMake({ 'makeprg': makeprg, 'errorformat': errorformat, 'subtype': 'Style' })
-endfunction
-
-"Helper function. This one runs and parses phpmd tool output.
-function! s:GetPHPMDErrors()
-    let makeprg = syntastic#makeprg#build({
-                \ 'exe': 'phpmd',
-                \ 'post_args': 'text ' . g:syntastic_phpmd_rules,
-                \ 'subchecker': 'phpmd' })
-    let errorformat = '%E%f:%l%m'
-    return SyntasticMake({ 'makeprg': makeprg, 'errorformat': errorformat, 'subtype' : 'Style' })
+    return SyntasticMake({ 'makeprg': makeprg, 'errorformat': errorformat })
 endfunction
 
 call g:SyntasticRegistry.CreateAndRegisterChecker({

syntax_checkers/php/phpmd.vim
@@ -1,0 +1,31 @@
+"============================================================================
+"File:        phpmd.vim
+"Description: Syntax checking plugin for syntastic.vim
+"Maintainer:  Martin Grenfell <martin.grenfell at gmail dot com>
+"License:     This program is free software. It comes without any warranty,
+"             to the extent permitted by applicable law. You can redistribute
+"             it and/or modify it under the terms of the Do What The Fuck You
+"             Want To Public License, Version 2, as published by Sam Hocevar.
+"             See http://sam.zoy.org/wtfpl/COPYING for more details.
+"
+"============================================================================
+"
+" See here for details of phpmd
+"   - phpmd (see http://phpmd.org)
+
+function! SyntaxCheckers_php_phpmd_IsAvailable()
+    return executable('phpmd')
+endfunction
+
+function! SyntaxCheckers_php_phpmd_GetLocList()
+    let makeprg = syntastic#makeprg#build({
+                \ 'exe': 'phpmd',
+                \ 'post_args': 'text  codesize,design,unusedcode,naming',
+                \ 'subchecker': 'phpmd' })
+    let errorformat = '%E%f:%l%m'
+    return SyntasticMake({ 'makeprg': makeprg, 'errorformat': errorformat, 'subtype' : 'Style' })
+endfunction
+
+call g:SyntasticRegistry.CreateAndRegisterChecker({
+    \ 'filetype': 'php',
+    \ 'name': 'phpmd'})

commit 9a3d667f15e0f279fc16ebcbb417289c2ea19c20

Fixed: Missing PHPCS configuration variable.
653611c introduced that regression during a refactoring.

syntax_checkers/php/phpcs.vim
@@ -13,6 +13,12 @@
 " See here for details of phpcs
 "    - phpcs (see http://pear.php.net/package/PHP_CodeSniffer)
 "
+
+"Support passing configuration directives to phpcs
+if !exists("g:syntastic_phpcs_conf")
+    let g:syntastic_phpcs_conf = ""
+endif
+
 function! SyntaxCheckers_php_phpcs_IsAvailable()
     return executable('phpcs')
 endfunction
@@ -20,7 +26,7 @@ endfunction
 function! SyntaxCheckers_php_phpcs_GetLocList()
     let makeprg = syntastic#makeprg#build({
                 \ 'exe': 'phpcs',
-                \ 'args': '--report=csv',
+                \ 'args': g:syntastic_phpcs_conf . ' --report=csv',
                 \ 'subchecker': 'phpcs' })
     let errorformat = '%-GFile\,Line\,Column\,Type\,Message\,Source\,Severity,"%f"\,%l\,%c\,%t%*[a-zA-Z]\,"%m"\,%*[a-zA-Z0-9_.-]\,%*[0-9]'
     return SyntasticMake({ 'makeprg': makeprg, 'errorformat': errorformat, 'subtype': 'Style' })