c55384c0ec05f32ead72fc2a9f5328119ac0da98
Ahmad Sherif <me@ahmadsherif.com>
2014-09-01 17:53:16 +0300

    Transform code using pmod_pt module before checking
    Helps suppressing the error "parameterized modules are no longer supported" which libraries like ChicagoBoss depend on.

--- syntax_checkers/erlang/erlang_check_file.erl
+++ syntax_checkers/erlang/erlang_check_file.erl
@@ -45,7 +45,12 @@ compile(FileName, LibDirs) ->
                   warn_export_vars,
                   strong_validation,
                   report] ++
-                 [{i, filename:join(Root, I)} || I <- LibDirs]).
+                 [{i, filename:join(Root, I)} || I <- LibDirs] ++
+                 case lists:member("deps/pmod_transform/include", LibDirs) of
+                     true -> [{parse_transform, pmod_pt}];
+                     _    -> []
+                 end
+                ).
 
 get_root(Dir) ->
     Path = filename:split(filename:absname(Dir)),
